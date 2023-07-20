73f6f7f75f53b43b454ec96a1ac906016b3a8444
Moamin <moamin.salamah@gmail.com>
2023-07-20 14:13:18 +0200

    sidebar component done

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./body.scss";
-
+import Top from "./top/Top";
+import Listing from "./listing/Listing";
+import Activity from "./activity/Activity";
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div className="mainContent">
+      <Top />
+      <div className="bottom flex">
+        <Listing />
+        <Activity />
+      </div>
+    </div>
+  );
 };
 
 export default Body;
